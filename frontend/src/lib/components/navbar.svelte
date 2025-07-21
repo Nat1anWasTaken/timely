a5e6ec4cc7dfe97ef1f631c854ac1f3f7f22723e
Nat1anWasTaken <nathan@nat1an.xyz>
2025-07-21 23:18:58 +0800

    feat: add registration form and update layout with navbar and theme toggle

---
+++ frontend/src/lib/components/navbar.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Card, CardTitle } from "$lib/components/ui/card/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import ModeToggle from "$lib/components/mode-toggle.svelte";
+</script>
+
+<div class="m-4 flex max-h-16">
+    <Card class="w-full flex-row items-center justify-between p-4">
+        <CardTitle class="text-xl">Timely</CardTitle>
+        <div class="flex items-center">
+            <ModeToggle />
+            <Button variant="outline" class="ml-4" href="/login">Login</Button>
+        </div>
+    </Card>
+</div>
+<x></x>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-	import '../app.css';
+    import "../app.css";
+    import { page } from "$app/state";
+    import Navbar from "$lib/components/navbar.svelte";
+    import { ModeWatcher } from "mode-watcher";
 
-	let { children } = $props();
+    let { children } = $props();
+
+    let hideNavbar = $derived(["/login", "/register"].includes(page.url.pathname));
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="flex h-screen w-screen flex-col">
+    {#if !hideNavbar}
+        <Navbar />
+    {/if}
+
+    <div class="h-full w-full flex-1 overflow-y-auto">
+        {@render children()}
+    </div>
+</div>
